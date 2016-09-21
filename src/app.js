b66afca23427e2928e9762fb38fa07a0125b2558
laurenkl96 <lklynch@ksu.edu>
2016-09-20 22:47:56 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,10 +6,18 @@ const Game = require('./game');
 /* Global variables */
 var canvas = document.getElementById('screen');
 var game = new Game(canvas, update, render);
+var image = new Image();
+image.src='assets/animals.png';
 
 // We have 9 pairs of possible cards that are about 212px square
 var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 var board = [];
+while(cards.length > 0){
+  var index = Math.floor(Math.random()*(cards.length-1));
+  board.push({card: cards[index], flip:false});
+  cards.splice(index,1);
+}
+console.log(board);
 
 // TODO: Place the cards on the board in random order
 
@@ -55,4 +63,16 @@ function render(elapsedTime, ctx) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // TODO: Render the board
+  for(var y = 0; y<3; y++) { 
+    for(var x = 0; x<6; x++){
+      var card = board[y*6+x];
+      if(card.flip){
+        ctx.drawImage(image,(card.card % 3 *212), (Math.floor(card.card*3)),0,212,212,(x*165+3),(y*165+3),160,160);
+      }
+      else{
+        ctx.fillStyle= "#800080";
+        ctx.fillRect(x*165+3, y*165+3, 160, 160);
+      }
+    }
+  }
 }
